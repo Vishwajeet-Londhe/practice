c5b16be6a1d467158756ed2317fc51641369fcd5
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-03 17:59:12 +0530

    for loop

--- .vscode/script.js
+++ .vscode/script.js
@@ -11,10 +11,14 @@
 // var b={...a};
 // b.name="b";
 
-var a=12;
-var b=13;
-var c=a+b;
-var d=a-b;
-var e=a/b;
-var f=a*b;
-var g=a%b;
+// var a=12;
+// var b=13;
+// var c=a+b;
+// var d=a-b;
+// var e=a/b;
+// var f=a*b;
+// var g=a%b;
+
+for(var i=0; i<12; i++){
+    console.log(i);
+}
