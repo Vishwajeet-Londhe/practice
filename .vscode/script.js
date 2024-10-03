cdab5cfe166190a89a509e2dea060e596df65818
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-03 17:56:17 +0530

    js

---
+++ .vscode/script.js
@@ -0,0 +1,20 @@
+// var b = [1,2,3];
+// var d = b;
+// d.pop();
+
+// var b=[1,2,3];
+// var d=[...b];
+
+// d.pop();
+
+// var a= {name:"a", type:"pastic"};
+// var b={...a};
+// b.name="b";
+
+var a=12;
+var b=13;
+var c=a+b;
+var d=a-b;
+var e=a/b;
+var f=a*b;
+var g=a%b;
